f8688e2410272177cdb5e8038e0d006e69c10378
Gregori Nunes <gregorisnunes@outlook.com>
2024-01-03 20:24:15 -0300

    feat: adicionando classe endereco

---
+++ backend/search-organic/src/endereco/Endereco.java
@@ -0,0 +1,96 @@
+package endereco;
+
+import java.util.UUID;
+
+public class Endereco {
+    private final UUID id;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String cep;
+    private String cidade;
+    private String estado;
+    private String pais;
+
+    public Endereco(String logradouro, String numero, String complemento, String cep, String cidade, String estado, String pais) {
+        this.id = UUID.randomUUID();
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.pais = pais;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public void imprimir() {
+        System.out.printf("ID: %s\n", this.id);
+        System.out.printf("Logradouro: %s\n", this.logradouro);
+        System.out.printf("Número: %s\n", this.numero);
+        System.out.printf("Complemento: %s\n", this.complemento);
+        System.out.printf("CEP: %s\n", this.cep);
+        System.out.printf("Cidade: %s\n", this.cidade);
+        System.out.printf("Estado: %s\n", this.estado);
+        System.out.printf("País: %s\n", this.pais);
+    }
+}
