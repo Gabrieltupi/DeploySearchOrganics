b80e4a8b2ee7a3dbaab539cd2d05dd736a96d43d
Anderson Luiz <trkz.anderson@gmail.com>
2024-02-01 17:50:09 -0300

    feat: adiciona jpa para endpoints principais de empresa

--- src/main/java/com/vemser/dbc/searchorganic/service/EmpresaService.java
+++ src/main/java/com/vemser/dbc/searchorganic/service/EmpresaService.java
@@ -1,14 +1,12 @@
 package com.vemser.dbc.searchorganic.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vemser.dbc.searchorganic.dto.empresa.CreateEmpresaDTO;
 import com.vemser.dbc.searchorganic.dto.empresa.EmpresaDTO;
 import com.vemser.dbc.searchorganic.dto.empresa.UpdateEmpresaDTO;
-import com.vemser.dbc.searchorganic.exceptions.BancoDeDadosException;
 import com.vemser.dbc.searchorganic.exceptions.RegraDeNegocioException;
 import com.vemser.dbc.searchorganic.model.Empresa;
-import com.vemser.dbc.searchorganic.model.Produto;
 import com.vemser.dbc.searchorganic.repository.EmpresaRepository;
-import com.vemser.dbc.searchorganic.repository.ProdutoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,71 +16,60 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EmpresaService {
     private final EmpresaRepository empresaRepository;
-    private final ProdutoRepository produtoRepository;
     private final ObjectMapper objectMapper;
 
-    private final ProdutoService produtoService;
+    public List<EmpresaDTO> findAll() throws Exception {
+        List<Empresa> pessoasEntity =  empresaRepository.findAll();
 
-
-    public Empresa adicionarEmpresa(Integer idUsuario, Empresa empresa) throws BancoDeDadosException {
-        return empresaRepository.adicionar(empresa, idUsuario);
+        return objectMapper.convertValue(pessoasEntity, objectMapper.getTypeFactory().constructCollectionType(List.class, EmpresaDTO.class));
     }
 
-    public Empresa buscarEmpresa(Integer id) throws BancoDeDadosException, RegraDeNegocioException {
-
-        return empresaRepository.buscaPorId(id);
-
-
+    public EmpresaDTO findById(Integer idEmpresa) throws Exception {
+        return retornarDTO(empresaRepository.findById(idEmpresa).orElseThrow(() -> new RegraDeNegocioException("Não encontrado")));
     }
 
-//    public Empresa atualizarEmpresa(Integer idEmpresa, Empresa novaEmpresa) throws Exception {
-//        empresaRepository.buscaPorId(idEmpresa);
-//        return empresaRepository.editar(idEmpresa, novaEmpresa);
+    public EmpresaDTO save(Integer idUsuario, CreateEmpresaDTO empresaDto) {
+        Empresa empresa = objectMapper.convertValue(empresaDto, Empresa.class);
+        empresa.setIdUsuario(idUsuario);
+
+        return retornarDTO(empresaRepository.save(empresa));
+    }
+
+    public EmpresaDTO update(Integer idEmpresa, UpdateEmpresaDTO empresaDto) throws Exception {
+        Empresa empresa = objectMapper.convertValue(empresaDto, Empresa.class);
+        empresa.setIdEmpresa(idEmpresa);
+
+        return retornarDTO(empresaRepository.save(empresa));
+    }
+
+    public void delete(Integer idEmpresa) {
+        empresaRepository.deleteById(idEmpresa);
+    }
+
+//    public EmpresaDTO preencherInformacoes(Empresa empresa) throws Exception {
+//        EmpresaDTO empresaDTO = new EmpresaDTO();
+//        empresaDTO.setIdEmpresa(empresa.getIdEmpresa());
+//        empresaDTO.setIdUsuario(empresa.getIdUsuario());
+//        empresaDTO.setProdutos(empresa.getProdutos());
+//        empresaDTO.setNomeFantasia(empresa.getNomeFantasia());
+//        empresaDTO.setInscricaoEstadual(empresa.getInscricaoEstadual());
+//        empresaDTO.setCnpj(empresa.getCnpj());
+//        empresaDTO.setSetor(empresa.getSetor());
+//        empresaDTO.setRazaoSocial(empresa.getRazaoSocial());
+//        return empresaDTO;
+//    }
+//
+//    public List<Produto> listarProdutosDaLoja(Integer idEmpresa) throws BancoDeDadosException, RegraDeNegocioException {
+//        Empresa empresa = empresaRepository.buscaPorId(idEmpresa);
+//
+//        if (empresa != null) {
+//            return produtoRepository.listarProdutosLoja(idEmpresa);
+//        }
+//        return List.of();
 //    }
 
-
-    public Empresa atualizarEmpresa(Integer idEmpresa, UpdateEmpresaDTO novaEmpresa) throws Exception {
-        Empresa empresa = buscarEmpresa(idEmpresa);
-        empresa.setNomeFantasia(novaEmpresa.getNomeFantasia());
-        empresa.setInscricaoEstadual(novaEmpresa.getInscricaoEstadual());
-        empresa.setCnpj(novaEmpresa.getCnpj());
-        empresa.setSetor(novaEmpresa.getSetor());
-        empresa.setRazaoSocial(novaEmpresa.getRazaoSocial());
-
-        return empresaRepository.editar(idEmpresa, empresa);
-    }
-
-    public void excluirEmpresa(int id) {
-        try {
-            empresaRepository.remover(id);
-            System.out.println("Empresa com o ID " + id + " excluída com sucesso.");
-        } catch (BancoDeDadosException bdEx) {
-            throw new RuntimeException(bdEx.getMessage());
-        } catch (Exception e) {
-            System.err.println("Erro ao excluir empresa: " + e.getMessage());
-        }
-    }
-
-    public EmpresaDTO preencherInformacoes(Empresa empresa) throws Exception {
-        EmpresaDTO empresaDTO = new EmpresaDTO();
-        empresaDTO.setIdEmpresa(empresa.getIdEmpresa());
-        empresaDTO.setIdUsuario(empresa.getIdUsuario());
-        empresaDTO.setProdutos(empresa.getProdutos());
-        empresaDTO.setNomeFantasia(empresa.getNomeFantasia());
-        empresaDTO.setInscricaoEstadual(empresa.getInscricaoEstadual());
-        empresaDTO.setCnpj(empresa.getCnpj());
-        empresaDTO.setSetor(empresa.getSetor());
-        empresaDTO.setRazaoSocial(empresa.getRazaoSocial());
-        return empresaDTO;
-    }
-
-    public List<Produto> listarProdutosDaLoja(Integer idEmpresa) throws BancoDeDadosException, RegraDeNegocioException {
-        Empresa empresa = empresaRepository.buscaPorId(idEmpresa);
-
-        if (empresa != null) {
-            return produtoRepository.listarProdutosLoja(idEmpresa);
-        }
-        return List.of();
+    private EmpresaDTO retornarDTO(Empresa entity) {
+        return objectMapper.convertValue(entity, EmpresaDTO.class);
     }
 }
 
