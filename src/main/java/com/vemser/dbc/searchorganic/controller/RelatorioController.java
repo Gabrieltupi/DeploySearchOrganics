b4c44fe2eb07f486d09c046b4b151c66217abb43
Anderson Luiz <trkz.anderson@gmail.com>
2024-02-05 12:57:23 -0300

    feat: adiciona relatorio de produtos x pedidos por mes

--- src/main/java/com/vemser/dbc/searchorganic/controller/RelatorioController.java
+++ src/main/java/com/vemser/dbc/searchorganic/controller/RelatorioController.java
@@ -2,6 +2,7 @@ package com.vemser.dbc.searchorganic.controller;
 
 import com.vemser.dbc.searchorganic.controller.interfaces.IRelatorioController;
 import com.vemser.dbc.searchorganic.dto.relatorio.RelatorioProdutoPedidosDTO;
+import com.vemser.dbc.searchorganic.dto.relatorio.RelatorioProdutoPedidosMesDTO;
 import com.vemser.dbc.searchorganic.dto.relatorio.RelatorioProdutoPrecoDTO;
 import com.vemser.dbc.searchorganic.dto.relatorio.RelatorioProdutoQuantidadeDTO;
 import com.vemser.dbc.searchorganic.service.RelatorioService;
@@ -33,5 +34,10 @@ public class RelatorioController implements IRelatorioController {
     @GetMapping("/produto/pedidos")
     public ResponseEntity<Page<RelatorioProdutoPedidosDTO>> findAllProdutosByPedidos(@PageableDefault(page = 0, size = 10, sort = "pedidos", direction = Sort.Direction.DESC) Pageable pageable) throws Exception {
         return new ResponseEntity<>(relatorioService.findAllProdutosByPedidos(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping("/produto/pedidos/mes")
+    public ResponseEntity<Page<RelatorioProdutoPedidosMesDTO>> findAllProdutosByPedidosByMes(@PageableDefault(page = 0, size = 10, sort = "mes", direction = Sort.Direction.DESC) Pageable pageable) throws Exception {
+        return new ResponseEntity<>(relatorioService.findAllProdutosByPedidosByMes(pageable), HttpStatus.OK);
     }
 }
